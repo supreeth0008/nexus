77dec322043c5f4723ecb4f9afe289bf3fc78cc8
Supreeth Bhat <supreeth0008@users.noreply.github.com>
2026-07-10 06:14:43 +0000

    style(utils): reformat version module

--- nexus/utils/version.py
+++ nexus/utils/version.py
@@ -1,6 +1,14 @@
 import sys
+
 VERSION = "1.0.0"
 COMMIT = "prod-ready"
 DATE = "2026-07-08"
+
+
 def build_info() -> dict:
-    return {"version": VERSION, "commit": COMMIT, "date": DATE, "python_version": sys.version.split()[0]}
+    return {
+        "version": VERSION,
+        "commit": COMMIT,
+        "date": DATE,
+        "python_version": sys.version.split()[0],
+    }
